a3a1e6a887e3d1308838e50a71af84aaab2d738e
gabriel natan <gabriel--natan@live.com>
2023-09-15 23:52:31 -0300

    feat: add initialization of database connection before starting application

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 import express, { Request, Response } from 'express';
 import 'reflect-metadata';
 import AppError from '@shared/errors/AppError';
+import { AppDataSource } from '@shared/typeorm';
 
-const app = express();
+AppDataSource.initialize().then(() => {
+  const app = express();
 
-app.use((error: Error, request: Request, response: Response) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
+  app.use((error: Error, request: Request, response: Response) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
       status: 'error',
       message: error.message,
     });
-  }
-
-  console.log(error);
-
-  return response.status(500).json({
-    status: 'error',
-    message: error.message,
   });
-});
 
-app.listen(3000, () => {
-  console.log('App runner');
+  app.listen(3000, () => {
+    console.log('App runner');
+  });
 });
