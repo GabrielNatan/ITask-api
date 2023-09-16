741e42f99fa48adba97ba1ea64b4b6c934909e32
gabriel natan <gabriel--natan@live.com>
2023-09-16 15:40:43 -0300

    feat: create user migration from user table

---
+++ src/shared/typeorm/migrations/1694833942903-Users.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Users1694833942903 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'serial',
+            isPrimary: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'role',
+            type: 'enum',
+            enum: ['admin', 'employed'],
+          },
+          {
+            name: 'first_name',
+            type: 'varchar',
+          },
+          {
+            name: 'last_name',
+            type: 'varchar',
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
