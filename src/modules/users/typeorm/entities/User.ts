67c84f661d5a782173616a4b8e76469856c90ef9
gabriel natan <gabriel--natan@live.com>
2023-09-17 12:04:26 -0300

    feat: create User

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  EMPLOYED = 'employed',
+}
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('varchar')
+  first_name: string;
+
+  @Column('varchar')
+  last_name: string;
+
+  @Column('varchar')
+  email: string;
+
+  @Column('varchar')
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.EMPLOYED,
+  })
+  role: UserRole;
+
+  @Column('bool')
+  active: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
