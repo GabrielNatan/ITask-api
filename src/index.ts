b6df4d072d694080cb6b7ccfd47088d5508f4e02
gabriel natan <gabriel--natan@live.com>
2023-09-17 15:02:13 -0300

    fix: add last parameter in error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,39 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'reflect-metadata';
 import AppError from '@shared/errors/AppError';
 import { AppDataSource } from '@shared/typeorm';
+import { usersRoutes } from '@modules/users/routes/users.routes';
 
 AppDataSource.initialize().then(() => {
   const app = express();
 
-  app.use((error: Error, request: Request, response: Response) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
+  app.use(express.json());
+
+  app.use('/users', usersRoutes);
+
+  app.use(
+    (
+      error: Error,
+      request: Request,
+      response: Response,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      _next: NextFunction
+    ) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+
+      console.log(error);
+
+      return response.status(500).json({
         status: 'error',
         message: error.message,
       });
     }
-
-    console.log(error);
-
-    return response.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  });
+  );
 
   app.listen(3000, () => {
     console.log('App runner');
