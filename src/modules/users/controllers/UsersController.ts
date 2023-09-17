9a22cce6d3077c82ff0ac3d3993d4347cdced79c
gabriel natan <gabriel--natan@live.com>
2023-09-17 15:30:56 -0300

    feat: feat: create user update method in UsersController

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserServices';
+import UpdateUserServices from '../services/UpdateUserServices';
 
 class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -7,6 +8,22 @@ class UsersController {
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({
+      first_name,
+      last_name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { first_name, last_name, email, password } = request.body;
+    const updateUserService = new UpdateUserServices();
+
+    const user = await updateUserService.execute({
+      id: Number(id),
       first_name,
       last_name,
       email,
