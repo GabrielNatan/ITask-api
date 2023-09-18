8bf7f086848283da400b72519918e4c96df7774f
gabriel natan <gabriel--natan@live.com>
2023-09-17 22:48:16 -0300

    feat: create mothods index, show and delete in controller user

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserServices';
 import UpdateUserServices from '../services/UpdateUserServices';
+import ShowUserService from '../services/ShowUserService';
+import ListUserService from '../services/ListUserService';
+import DeleteUserService from '../services/DeleteUserService';
 
 class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUserService = new ListUserService();
+    const users = await listUserService.execute();
+    return response.json(users);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showUserService = new ShowUserService();
+
+    const user = await showUserService.execute({ id: Number(id) });
+
+    return response.json(user);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { first_name, last_name, email, password } = request.body;
     const createUserService = new CreateUserService();
@@ -31,6 +49,16 @@ class UsersController {
     });
 
     return response.json(user);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteUserService = new DeleteUserService();
+
+    await deleteUserService.execute(Number(id));
+
+    return response.status(201).send();
   }
 }
 
