70cd6d9c24ab9b687f075d6d352a6669fddca430
gabriel natan <gabriel--natan@live.com>
2023-09-20 23:49:15 -0300

    feat: create service to delete customer.

---
+++ src/modules/customer/services/UpdateCustomerService.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+import Customer from '../typeorm/entities/Customer';
+import { customerRepository } from '../typeorm/repositories/CustomerRepository';
+
+interface IResponse {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}
+
+class UpdateCustomerService {
+  public async execute({
+    id,
+    email,
+    first_name,
+    last_name,
+    password,
+  }: IResponse): Promise<Customer> {
+    const customerExists = await customerRepository.findById(Number(id));
+
+    if (!customerExists) {
+      throw new AppError('Customer not found', 404);
+    }
+
+    customerExists.email = email;
+    customerExists.password = password;
+    customerExists.first_name = first_name;
+    customerExists.last_name = last_name;
+
+    await customerRepository.save(customerExists);
+
+    return customerExists;
+  }
+}
+
+export default UpdateCustomerService;
