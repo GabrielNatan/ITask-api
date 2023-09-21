66180469b2c4b049cd8ec24848a02697426ee2db
gabriel natan <gabriel--natan@live.com>
2023-09-20 23:49:46 -0300

    feat: create CustomerRepository.

---
+++ src/modules/customer/typeorm/repositories/CustomerRepository.ts
@@ -0,0 +1,45 @@
+import { EntityManager, EntityTarget, QueryRunner, Repository } from 'typeorm';
+import Customer from '../entities/Customer';
+import { AppDataSource } from '@shared/typeorm';
+
+class CustomerRepository extends Repository<Customer> {
+  constructor(
+    customer: EntityTarget<Customer>,
+    manager: EntityManager,
+    queryRunner: QueryRunner | undefined
+  ) {
+    super(customer, manager, queryRunner);
+  }
+
+  public async findById(id: number) {
+    return this.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  public async findByName(first_name: string) {
+    return this.findOne({
+      where: {
+        first_name,
+      },
+    });
+  }
+
+  public async findByEmail(email: string) {
+    return this.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+}
+
+export const customerRepository = new CustomerRepository(
+  Customer,
+  AppDataSource.createEntityManager(),
+  AppDataSource.manager.queryRunner
+);
+
+export default CustomerRepository;
