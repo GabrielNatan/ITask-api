b64238cae40f37409431c8264841f9402256d66d
gabriel natan <gabriel--natan@live.com>
2023-09-24 20:29:15 -0300

    feat: add methods in ICustomerRepository

--- src/modules/customer/domain/repositories/ICustomerRepository.ts
+++ src/modules/customer/domain/repositories/ICustomerRepository.ts
@@ -1,5 +1,9 @@
+import { ICreateCustomer } from '../models/ICreateCustomer';
 import { ICustomer } from '../models/ICustomer';
 
 export interface ICustomerRepository {
-  findById(id: number): Promise<ICustomer | undefined>;
+  findById(id: number): Promise<ICustomer | null>;
+  findByName(first_name: string): Promise<ICustomer | null>;
+  findByEmail(email: string): Promise<ICustomer | null>;
+  create(data: ICreateCustomer): Promise<ICustomer>;
 }
