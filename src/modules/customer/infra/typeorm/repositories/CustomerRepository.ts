79ba01efd3df423482d420a3d5e0d47cc3a2794d
gabriel natan <gabriel--natan@live.com>
2023-09-24 20:30:44 -0300

    refactor:change methods in CustomerRepository to implement interface

--- src/modules/customer/infra/typeorm/repositories/CustomerRepository.ts
+++ src/modules/customer/infra/typeorm/repositories/CustomerRepository.ts
@@ -1,45 +1,60 @@
-import { EntityManager, EntityTarget, QueryRunner, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import Customer from '../entities/Customer';
 import { AppDataSource } from '@shared/infra/typeorm';
+import { ICustomerRepository } from '@modules/customer/domain/repositories/ICustomerRepository';
+import { ICustomer } from '@modules/customer/domain/models/ICustomer';
+import { ICreateCustomer } from '@modules/customer/domain/models/ICreateCustomer';
 
-class CustomerRepository extends Repository<Customer> {
-  constructor(
-    customer: EntityTarget<Customer>,
-    manager: EntityManager,
-    queryRunner: QueryRunner | undefined
-  ) {
-    super(customer, manager, queryRunner);
+class CustomerRepository implements ICustomerRepository {
+  private ormRepository: Repository<Customer>;
+  constructor() {
+    this.ormRepository = AppDataSource.getRepository(Customer);
+    console.log('ORM ', this.ormRepository);
   }
 
-  public async findById(id: number) {
-    return this.findOne({
+  public async findById(id: number): Promise<ICustomer | null> {
+    return this.ormRepository.findOne({
       where: {
         id,
       },
     });
   }
 
-  public async findByName(first_name: string) {
-    return this.findOne({
+  public async findByName(first_name: string): Promise<ICustomer | null> {
+    return this.ormRepository.findOne({
       where: {
         first_name,
       },
     });
   }
 
-  public async findByEmail(email: string) {
-    return this.findOne({
+  public async findByEmail(email: string): Promise<ICustomer | null> {
+    return this.ormRepository.findOne({
       where: {
         email,
       },
     });
   }
+
+  public async create({
+    email,
+    first_name,
+    last_name,
+    password,
+  }: ICreateCustomer): Promise<ICustomer> {
+    const customer = this.ormRepository.create({
+      email,
+      first_name,
+      last_name,
+      password,
+    });
+
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
 }
 
-export const customerRepository = new CustomerRepository(
-  Customer,
-  AppDataSource.createEntityManager(),
-  AppDataSource.manager.queryRunner
-);
+export const customerRepository = new CustomerRepository();
 
 export default CustomerRepository;
